b8b31d7da829d01b7553176049e8aedcd2fe563f
laznar <laznar@uninorte.edu.co>
2021-04-25 12:07:22 -0500

    Change to camelCase and Uppercase

--- src/views/prueba.jsx
+++ src/views/prueba.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import PruebaComponent from "./../components/prueba/pruebaComponent";
+import ButtonComponent from "./../components/prueba/buttonComponent";
 
 //Components
 
-function prueba() {
+function Prueba() {
   return (
     <div>
       <h1>Hola Mundo</h1>
+      <PruebaComponent />
+      <ButtonComponent />
     </div>
   );
 }
 
-export default prueba;
+export default Prueba;
