941a27dc6e3ca18d5f32a4a0e01f99bf2a8e6d7d
CPallares96 <urielesc@uninorte.edu.co>
2021-04-29 20:55:58 -0500

    29/04/2021 Calendario

--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,6 +1,8 @@
+//
 import React from "react";
 import Home from "./Home";
 import Prueba from "./Prueba";
+import Agenda from "./Agenda";
 import { Switch, Route, Redirect } from "react-router-dom";
 import "../fontawesome";
 
@@ -9,6 +11,7 @@ function Main() {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/prueba" component={Prueba} />
+      <Route exact path="/agenda" component={Agenda} />
       <Redirect to="/" />
     </Switch>
   );
