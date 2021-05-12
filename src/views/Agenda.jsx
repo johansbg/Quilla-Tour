3007d6bfebbfe8859f4454ba1e5196acc084a49b
CPallares96 <ferch5003@gmail.com>
2021-05-12 18:39:06 -0500

    cambio mono

--- src/views/Agenda.jsx
+++ src/views/Agenda.jsx
@@ -1,55 +1,158 @@
-import {useState, useCallback, useMemo, useEffect} from "react"
-import "@mobiscroll/react/dist/css/mobiscroll.min.css";
-import React from "react"
-import { Eventcalendar, getJson, toast, localeEs } from '@mobiscroll/react';
+import { useState, useCallback, useMemo, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  Resources,
+  WeekView,
+  Appointments,
+  AppointmentTooltip,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import { withStyles } from "@material-ui/core/styles";
+
+const appointments = [
+  {
+    title: "Website Re-Design Plan",
+    startDate: new Date(2018, 5, 25, 12, 35),
+    endDate: new Date(2018, 5, 25, 15, 0),
+    id: 0,
+    members: [1, 3, 5],
+    location: "Room 1",
+  },
+  {
+    title: "Book Flights to San Fran for Sales Trip",
+    startDate: new Date(2018, 5, 26, 12, 35),
+    endDate: new Date(2018, 5, 26, 15, 0),
+    id: 1,
+    members: [2, 4],
+    location: "Room 2",
+  },
+  {
+    title: "Install New Router in Dev Room",
+    startDate: new Date(2018, 5, 27, 12, 35),
+    endDate: new Date(2018, 5, 27, 15, 0),
+    id: 2,
+    members: [3],
+    location: "Room 3",
+  },
+  {
+    title: "Approve Personal Computer Upgrade Plan",
+    startDate: new Date(2018, 5, 28, 12, 35),
+    endDate: new Date(2018, 5, 28, 15, 0),
+    id: 3,
+    members: [4, 1],
+    location: "Room 4",
+  },
+  {
+    title: "Final Budget Review",
+    startDate: new Date(2018, 5, 29, 12, 35),
+    endDate: new Date(2018, 5, 29, 15, 0),
+    id: 4,
+    members: [5, 1, 3],
+    location: "Room 5",
+  },
+];
+
+const styles = (theme) => ({
+  container: {
+    display: "flex",
+    marginBottom: theme.spacing(2),
+    justifyContent: "flex-end",
+  },
+  text: {
+    ...theme.typography.h6,
+    marginRight: theme.spacing(2),
+  },
+});
+
+const ResourceSwitcher = withStyles(styles, { name: "ResourceSwitcher" })(
+  ({ mainResourceName, onChange, classes, resources }) => (
+    <div className={classes.container}>
+      <div className={classes.text}>Main resource name:</div>
+      <Select
+        value={mainResourceName}
+        onChange={(e) => onChange(e.target.value)}
+      >
+        {resources.map((resource) => (
+          <MenuItem key={resource.fieldName} value={resource.fieldName}>
+            {resource.title}
+          </MenuItem>
+        ))}
+      </Select>
+    </div>
+  )
+);
+
+const initialState = {
+  data: appointments,
+  mainResourceName: "members",
+  resources: [
+    {
+      fieldName: "location",
+      title: "Location",
+      instances: [
+        { id: "Room 1", text: "Room 1" },
+        { id: "Room 2", text: "Room 2" },
+        { id: "Room 3", text: "Room 3" },
+        { id: "Room 4", text: "Room 4" },
+        { id: "Room 5", text: "Room 5" },
+      ],
+    },
+    {
+      fieldName: "members",
+      title: "Members",
+      allowMultiple: true,
+      instances: [
+        { id: 1, text: "Andrew Glover" },
+        { id: 2, text: "Arnie Schwartz" },
+        { id: 3, text: "John Heart" },
+        { id: 4, text: "Taylor Riley" },
+        { id: 5, text: "Brad Farkus" },
+      ],
+    },
+  ],
+};
 
 function Agenda(props) {
+  const [ state, setState ] = useState(initialState);
 
-    const evento = [{"start":"2021-05-03T08:00:00.000Z","end":"2021-05-06T17:00:00.000Z","title":"Business of Software Conference","color":"#ff6d42"}];
+  const changeMainResource = (mainResourceName) => {
+    setState({ ...state, mainResourceName });
+  };
 
-    const [myEvents, setEvents] = useState(evento);
-    // useEffect(() => {
-    //     getJson('https://trial.mobiscroll.com/events/?vers=5', (evento) => {
-    //         setEvents(evento);
-    //     }, 'jsonp');
-    // }, []);
+  const { data, resources, mainResourceName } = state;
 
+  return (
+    <>
+      <ResourceSwitcher
+        resources={resources}
+        mainResourceName={mainResourceName}
+        onChange={changeMainResource}
+      />
 
-    const action = () =>{
-        props.history.push("/prueba")
-    }
-
-    const onEventClick = useCallback((event) => {
-        toast({
-            
-            message: event.event.title,
-            duration: 10000
-        });
-        console.log("hola!")
-        action()
-    }, []);
-    
-    const view = useMemo(() => {
-        return {
-            calendar: { labels: true }
-        };
-    }, []);
-
-    return (
-            <Eventcalendar
-                locale={localeEs}
-                theme="ios" 
-                themeVariant="light"
-                clickToCreate={false}
-                dragToCreate={false}
-                dragToMove={false}
-                dragToResize={false}
-                height={697}
-                data={myEvents}
-                view={view}
-                onEventClick={onEventClick}
-           />
-    ); 
+      <Paper>
+        <Scheduler
+          data={data}
+        >
+          <ViewState
+            defaultCurrentDate="2018-06-27"
+          />
+          <WeekView
+            startDayHour={11.5}
+            endDayHour={16}
+          />
+          <Appointments />
+          <AppointmentTooltip />
+          <Resources
+            data={resources}
+            mainResourceName={mainResourceName}
+          />
+        </Scheduler>
+      </Paper>
+    </>
+  );
 }
 
-export default Agenda
+export default Agenda;
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -9,16 +9,9 @@ import "../fontawesome";
 function Main() {
   return (
     <Switch>
-<<<<<<< HEAD
-      <Route exact path="/" component={Home} />
-      <Route exact path="/prueba" component={Prueba} />
-      <Route exact path="/agenda" component={Agenda} />
-      <Redirect to="/" />
-=======
       <Route exact path="/Quilla-Tour" component={Home} />
-      <Route exact path="/Quilla-Tour/prueba" component={Prueba} />
+      <Route exact path="/Quilla-Tour/agenda" component={Agenda} />
       <Redirect to="/Quilla-Tour" />
->>>>>>> ce7e71b655c34a7f937ba7439ed2b126327d5455
     </Switch>
   );
 }
