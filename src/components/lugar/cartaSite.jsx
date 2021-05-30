9ef38be529a8cecb2ab1e51a733510cf0604895a
DvC99 <danielvalenciacordero@gmail.com>
2021-05-30 16:01:40 -0500

    Se creo el componene Lugar y se terminado la estructura

--- src/components/lugar/cartaSite.jsx
+++ src/components/lugar/cartaSite.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import {Button,Card} from 'react-bootstrap';
+import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle, Button} from 'reactstrap';
 
-const cartaSite = ()  => {
+const CartaSite = (props)  => {
     return(
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
+       <div>
+           <Card>
+                <CardImg top width="100%" height="200px" src={props.urlImg.default} alt="Card image cap" />
+                <CardBody>
+                <CardTitle tag="h5">{props.data.title}</CardTitle>
+                <CardSubtitle tag="h6" className="mb-2 text-muted">{props.data.Subtitle}</CardSubtitle>
+                <CardText>{props.data.text}</CardText>
+                <Button>Button</Button>
+                </CardBody>
+            </Card>
+       </div>
     );
 }
 
 
-export default cartaSite;
+export default CartaSite;
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -6,6 +6,7 @@ import Comunidad from "./Comunidad";
 import Explora from "./Explora";
 import Perfil from "./Perfil";
 import Agenda from "./Agenda";
+import Lugar from "./Lugar";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import "../fontawesome";
 
@@ -15,9 +16,10 @@ function Main() {
       <Route exact path="/Quilla-Tour" component={Home}/>
       <Route exact path="/Quilla-Tour/Comunidad" component={Comunidad}/>
       <Route exact path="/Quilla-Tour/InformacionDeLaCiudad" component={Informacion}/>
-      <Route exact path="/Quilla-Tour/explora" component={Explora} />
+      <Route exact path="/Quilla-Tour/Explora" component={Explora} />
       <Route exact path="/Quilla-Tour/Perfil" component={Perfil} />
-      <Route exact path="/Quilla-Tour/agenda" component={Agenda} />
+      <Route exact path="/Quilla-Tour/Agenda" component={Agenda} />
+      <Route exact path="/Quilla-Tour/Lugar" component={Lugar} />
       <Redirect to="/Quilla-Tour" />
     </Switch>
   );
