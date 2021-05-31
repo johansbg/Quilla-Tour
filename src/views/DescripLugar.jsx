353f98dfeda291883bd9fae32292cf37cd998109
DvC99 <danielvalenciacordero@gmail.com>
2021-05-31 13:33:07 -0500

    Se creo el componente Descripcion del Lugar

--- src/views/DescripLugar.jsx
+++ src/views/DescripLugar.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+//Librerias
+import { Container, Row, Col } from 'reactstrap';
+//Componentes
+import NavbarBrand from "../components/home/NavbarComponent";
 
 const DescripLugar = (props)  => {
     console.log(props.location.dataDetalle.props)
@@ -6,7 +10,21 @@ const DescripLugar = (props)  => {
     console.log(props.location.dataDetalle.props.urlImg.default)
     return(
         <div>
-
+            <Container fluid = {true}>
+                <Row>
+                    <Col >
+                        <NavbarBrand color={"dark"}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md = {6} className="mt-4 p-0">
+                       
+                    </Col>
+                    <Col md = {6}className="mt-4 pr-3 p-0">
+                        
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
